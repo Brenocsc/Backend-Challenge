9b1de836cab3511aefa4f5cb44bf6bb1ccf8fe34
brenocostabanqi <breno.costa@banqi.com>
2021-08-04 10:35:48 -0300

    create swagger route

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import i18n from 'i18n'
 import bodyParser from 'body-parser'
+import swaggerUi from 'swagger-ui-express'
 import { routers } from './api'
 import { config } from './config'
+import * as swaggerDocument from './swagger.json'
 
 const app = express()
 
@@ -12,5 +14,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 i18n.configure(config.i18nConfig)
 
 app.use(routers)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(config.port)
